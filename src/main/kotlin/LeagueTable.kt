ce034d0073d1616b9e1631fcde80d387f623bf42
matthewp <matthew.pollard@mkodo.com>
2020-08-18 14:21:17 +0100

    table initialises with 0 in all columns

---
+++ src/main/kotlin/LeagueTable.kt
@@ -0,0 +1,30 @@
+class LeagueTable {
+    fun getPoints(team: String): Int {
+        return 0
+    }
+
+    fun getGoalsFor(team: String): Int {
+        return 0
+    }
+
+    fun getGoalsAgainst(team: String): Int {
+        return 0
+    }
+
+    fun getGoalDifference(team: String): Int {
+        return 0
+    }
+
+    fun getWins(team: String): Int {
+        return 0
+    }
+
+    fun getDraws(team: String): Int {
+        return 0
+    }
+
+    fun getLosses(team: String): Int {
+        return 0
+    }
+
+}
---
+++ src/test/kotlin/LeagueTableShould.kt
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LeagueTableShould {
+    @Test
+    internal fun `initialise with zero data for Manchester United`() {
+        val team = "Manchester United"
+        val leagueTable = LeagueTable()
+        assertZeroData(leagueTable, team)
+    }
+
+    private fun assertZeroData(leagueTable: LeagueTable, team: String) {
+        assertEquals(leagueTable.getPoints(team), 0)
+        assertEquals(leagueTable.getGoalsFor(team), 0)
+        assertEquals(leagueTable.getGoalsAgainst(team), 0)
+        assertEquals(leagueTable.getGoalDifference(team), 0)
+        assertEquals(leagueTable.getWins(team), 0)
+        assertEquals(leagueTable.getDraws(team), 0)
+        assertEquals(leagueTable.getLosses(team), 0)
+    }
+}
